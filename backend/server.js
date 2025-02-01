5b354ff59b61e1b6d43bdaac0be8b366122edaf8
Syeda Fizza <117969468+SFizzaR@users.noreply.github.com>
2025-02-02 04:51:50 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,16 @@ const connectDB = require("./configs/dbConnections");
 const errorHandler = require('./middleware/errorHandler');
 
 const dotenv = require("dotenv").config();
+const cors = require('cors');
+
+
 
 connectDB();
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/users", require("./routes/UserRoute"));
 app.use(errorHandler)

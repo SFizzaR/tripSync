2a007943084e576074f512e19bd31062b5dca559
Zehra Waqar <119837446+zayyscodes@users.noreply.github.com>
2025-02-02 21:58:12 +0500

    Add files via upload

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -2,10 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import './index.css';
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
-import Register from "./pages/Register";
-import Password from "./pages/password";
-import Dashboard from "./pages/dashboard";
+import SignUp from "./pages/createAccount";
 
 const AppRoutes = () => {
   return (
@@ -13,9 +10,6 @@ const AppRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/setpassword" element={<Password />} />
-      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 };

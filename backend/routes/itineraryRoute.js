48053cbac81697e5a121cffa0fbe435e1e639de3
Syeda Fizza <117969468+SFizzaR@users.noreply.github.com>
2025-02-09 20:57:38 +0500

    Add files via upload

---
+++ backend/routes/itineraryRoute.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const {createItinerary, updateItinerary, addPlaceToItinerary, addUserToItinerary} = require("../controller/itineraryController");
+const router = express.Router();
+const {protect} = require("../middleware/errorHandler")
+
+router.post('/CreateItinerary', protect, createItinerary);
+router.put('/:id ', updateItinerary);
+router.post('/:id/places', addPlaceToItinerary);
+router.post('/:id/users', addUserToItinerary);
+
+module.exports = router;

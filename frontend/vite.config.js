854e6f80f3a77748ce92490f76e6c8dcdf67fb38
Zahab Jahangir <167731882+zeejaycoding@users.noreply.github.com>
2025-02-09 18:20:53 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    
   ],
+  envPrefix: "VITE_", 
   assetsInclude: ['**/*.JPG', '**/*.jpg', '**/*.PNG', '**/*.png']
 })

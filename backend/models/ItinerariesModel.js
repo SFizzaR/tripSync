349268747dd48ee4aabe6d520bbb69bbf3863843
Syeda Fizza <117969468+SFizzaR@users.noreply.github.com>
2025-02-11 16:15:39 +0500

    Add files via upload

--- backend/models/ItinerariesModel.js
+++ backend/models/ItinerariesModel.js
@@ -3,22 +3,24 @@ const mongoose = require("mongoose")
 const itinerarySchema = mongoose.Schema(
     {
         users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User", required: true}], // Multiple users
-        places: [{ type: mongoose.Schema.Types.ObjectId, ref: "Place", required: true }], // At least one required
-        title: { type: String, required: true }, // Will be set dynamically
+        places: [{ type: mongoose.Schema.Types.ObjectId, ref: "Place" }], // At least one required
+        title: { type: String, required: true,  default: function () { return this.city; }}, // Will be set dynamically
         status: { type: String, enum: ["planning", "in-progress", "complete"], default: "planning" },
-        collaborative: { type: Boolean, default: false }
+        collaborative: { type: Boolean, default: false },
+        city: {type: String, required: true},
+        startDate: { type: Date }, // Required start date
+    endDate: { 
+        type: Date, 
+    },
+    budget: { 
+        type: String, 
+        enum: ["Luxury", "Economical", "Standard"],
+    },
+    admin: {
+      type: mongoose.Schema.Types.ObjectId, ref: "User", required: true
+    }
         
     }
 )
-
-itinerarySchema.pre("save", async function (next) {
-    if (this.places.length > 0) {
-      const firstPlace = await Place.findById(this.places[0]);
-      if (firstPlace) {
-        this.title = firstPlace.city; // Set title as the city of the first place
-      }
-    }
-    next();
-  });
 
 module.exports = mongoose.model("Itinerary", itinerarySchema);

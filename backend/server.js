3d089bf5e628a519ebdbc6142f3585fdf4504e3d
Syeda Fizza <117969468+SFizzaR@users.noreply.github.com>
2025-02-11 16:17:40 +0500

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use("/api/users", require("./routes/UserRoute"));
-app.use("/api/itineraries", require('./routes/itineraryRoute'))
-app.use(errorHandler)
+app.use("/api/itineraries", require('./routes/itineraryRoute'));
+app.use(errorHandler);
 
 app.listen(port, ()=>{
     console.log(`server running on port:  ${port}`)

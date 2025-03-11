f0227fd4a20280e140cad79215cd534fb05871b5
Syeda Fizza <117969468+SFizzaR@users.noreply.github.com>
2025-03-11 18:01:48 +0500

    Add files via upload

---
+++ backend/routes/invitationRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const { SendInvitation, acceptInvite, rejectInvite, cancelInvite } = require('../controller/invitationController')
+const { protect } = require('../middleware/errorHandler');
+router = express.Router()
+
+router.post('/', protect, SendInvitation);
+router.put('/accept/:inviteId', protect, acceptInvite);
+router.delete('/reject/:inviteId', protect, rejectInvite);
+router.delete('/cancel/:inviteId', protect, cancelInvite);
+
+module.exports = router;

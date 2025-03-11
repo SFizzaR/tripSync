1550fa0994d54dc3037ca46e78f8cd201a535e44
Syeda Fizza <117969468+SFizzaR@users.noreply.github.com>
2025-03-11 18:01:17 +0500

    Add files via upload

---
+++ backend/models/invitationsModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+
+const inivitationSchema = mongoose.Schema(
+    {
+        itinerary: { type: mongoose.Schema.Types.ObjectId, ref: "Itinerary", required: true },
+        sender_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+        reciver_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+        status: { type: String, enum: ['pending', 'accepted', 'declined', 'cancel'], default: "pending" },
+    }
+)
+
+module.exports = mongoose.model("Invitations", inivitationSchema)

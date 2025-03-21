76f229ccb6430521fa3b2ad4a618d4c718adc2fc
Zahab Jahangir <167731882+zeejaycoding@users.noreply.github.com>
2025-03-22 00:35:11 +0500

    Update UserRoute.js

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {loginUser, registerUser, getFirstname, getUsers} = require("../controller/UserController");
+const {loginUser, registerUser, getFirstname, getUsers,getAllUsersExceptCurrent} = require("../controller/UserController");
 const { protect } = require("../middleware/errorHandler");
 const jwt = require("jsonwebtoken");
 
@@ -11,5 +11,6 @@ router.post('/register', registerUser);
 router.post("/login", loginUser);
 router.get("/getname",protect,getFirstname);
 router.get("/search", getUsers);
+router.get("/all", protect,getAllUsersExceptCurrent);
 
 module.exports = router;

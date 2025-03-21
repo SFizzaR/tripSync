9fdadfe6aa50343f6b0573945c6f0f07167ff58f
Zahab Jahangir <167731882+zeejaycoding@users.noreply.github.com>
2025-03-22 00:35:46 +0500

    Update itineraryRoute.js

--- backend/routes/itineraryRoute.js
+++ backend/routes/itineraryRoute.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const {createItinerary,getSoloItineraries,getColabItineraries ,updateItinerary, addPlaceToItinerary, addUserToItinerary, deleteUser, deletePlace} = require("../controller/itineraryController");
 const router = express.Router();
@@ -5,7 +6,7 @@ const {protect} = require("../middleware/errorHandler")
 
 router.post('/CreateItinerary', protect, createItinerary);
 router.put('/:id', updateItinerary);
-router.post('/:Itineraryid/places/:placeId', addPlaceToItinerary);
+router.post('/:itineraryId/places/:placeId', addPlaceToItinerary);
 router.post('/:Itineraryid/users/:userId', addUserToItinerary);
 router.get("/solo", protect, getSoloItineraries);
 router.get("/colab", protect, getColabItineraries);

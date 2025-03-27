51df51e5127f7f2668f6869dcad66387c364f55c
Zahab Jahangir <167731882+zeejaycoding@users.noreply.github.com>
2025-03-27 12:37:16 +0500

    Update itineraryRoute.js

--- backend/routes/itineraryRoute.js
+++ backend/routes/itineraryRoute.js
@@ -1,12 +1,14 @@
+
 const express = require("express");
-const {createItinerary,getSoloItineraries,getColabItineraries ,updateItinerary, addPlaceToItinerary, addUserToItinerary, deleteUser, deletePlace} = require("../controller/itineraryController");
+const {createItinerary,getSoloItineraries,getColabItineraries ,updateItinerary, addPlaceToItinerary,getItineraryPlaces ,addUserToItinerary, deleteUser, deletePlace} = require("../controller/itineraryController");
 const router = express.Router();
 const {protect} = require("../middleware/errorHandler")
 
 router.post('/CreateItinerary', protect, createItinerary);
 router.put('/:id', updateItinerary);
-router.post("/:itineraryId/places/:placeId", addPlaceToItinerary);
+router.post('/:itineraryId/places/:placeId', addPlaceToItinerary);
 router.post('/:Itineraryid/users/:userId', addUserToItinerary);
+router.get("/:itineraryId/placeDisplay", getItineraryPlaces);
 router.get("/solo", protect, getSoloItineraries);
 router.get("/colab", protect, getColabItineraries);
 router.delete('/:itineraryId/remove-user/:userId', protect, deleteUser);

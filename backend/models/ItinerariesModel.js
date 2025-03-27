625da3aa27eac9513a58ac4bfe22a4080a5e518b
Zahab Jahangir <167731882+zeejaycoding@users.noreply.github.com>
2025-03-27 12:36:38 +0500

    Update ItinerariesModel.js

--- backend/models/ItinerariesModel.js
+++ backend/models/ItinerariesModel.js
@@ -2,24 +2,29 @@ const mongoose = require("mongoose")
 
 const itinerarySchema = mongoose.Schema(
     {
-        users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User", required: true}], // Multiple users
-        places: [{ type: mongoose.Schema.Types.ObjectId, ref: "Place" }], // At least one required
-        title: { type: String, required: true,  default: function () { return this.city; }}, // Will be set dynamically
+        users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }], // Multiple users
+        places: [
+            {
+                placeId: { type: String, required: true },
+                placeName: { type: String, required: true }
+            }
+        ],
+        title: { type: String, required: true, default: function () { return this.city; } }, // Will be set dynamically
         status: { type: String, enum: ["planning", "in-progress", "complete"], default: "planning" },
         collaborative: { type: Boolean, default: false },
-        city: {type: String, required: true},
+        city: { type: String, required: true },
         startDate: { type: Date }, // Required start date
-    endDate: { 
-        type: Date, 
-    },
-    budget: { 
-        type: String, 
-        enum: ["Luxury", "Economical", "Standard"],
-    },
-    admin: {
-      type: mongoose.Schema.Types.ObjectId, ref: "User", required: true
-    }
-        
+        endDate: {
+            type: Date,
+        },
+        budget: {
+            type: String,
+            enum: ["Luxury", "Economical", "Standard"],
+        },
+        admin: {
+            type: mongoose.Schema.Types.ObjectId, ref: "User", required: true
+        }
+
     }
 )
 

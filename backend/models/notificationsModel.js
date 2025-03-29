9d8fa8f9de049800160e9a974bf625f9df62eb78
Syeda Fizza <117969468+SFizzaR@users.noreply.github.com>
2025-03-29 08:29:47 +0500

    Add files via upload

---
+++ backend/models/notificationsModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const notificationSchema = mongoose.Schema(
+    {
+        user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+        invite_id: { type: mongoose.Schema.Types.ObjectId, ref: "Invitations", required: true },
+        type: { type: String, enum: ["invite_received", "invite_accepted", "invite_declined"], required: true },
+        message: { type: String, required: true },
+        read: { type: Boolean, default: false },
+        actions: { type: Object }, // Stores available actions (e.g., accept/decline for invites)
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Notifications", notificationSchema)

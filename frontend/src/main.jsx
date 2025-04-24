b0d748e04392ae0cd45b84b0cb031642df1f0e92
Zehra Waqar <119837446+zayyscodes@users.noreply.github.com>
2025-04-25 00:53:47 +0500

    Add files via upload

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import AppRoutes from './Routes';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <Router>
       <AppRoutes />
     </Router>
-  </React.StrictMode >,
-  document.getElementById('root')
-);
+  </StrictMode>,
+)

a4fa978f0bdc6b0057fcfd9d509174ba742a9324
Zahab Jahangir <167731882+zeejaycoding@users.noreply.github.com>
2025-04-28 21:13:46 +0500

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use("/api/users", require("./routes/UserRoute"));
 app.use("/api/itineraries", require('./routes/itineraryRoute'));
 app.use("/api/places", require("./routes/placesRoute"));
+app.use("/api/addplace",require('./routes/addplaceRoutes'));
 app.use("/api/invite", require('./routes/invitationRoutes'));
 app.use("/api/invite", require('./routes/invitationRoutes'));
 app.use("/api/notifications", require("./routes/notificationRoutes"));
